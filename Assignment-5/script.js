0bc4a6d46f0a8753e70258d4ddc5cf8e9e81bbfd
hanzlikhan <khangormani79@gmail.com>
2024-02-25 23:17:58 +0500

    update with assignment 05

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -13,3 +13,18 @@ function showContent(id) {
     }
     document.querySelector(`a[href="#${id}"]`).classList.add("active");
 }
+
+function toggleCards() {
+    var hiddenCards = document.querySelector('.hidden-cards');
+    var showMoreBtn = document.getElementById('showMoreBtn');
+
+    if (hiddenCards.style.display === 'none') {
+        hiddenCards.style.display = 'grid';
+        showMoreBtn.textContent = 'Show Less';
+        // hiddenCards.classList.add('visible-cards'); // Add a class to the hidden cards
+    } else {
+        hiddenCards.style.display = 'none';
+        showMoreBtn.textContent = 'Show More';
+        // hiddenCards.classList.remove('visible-cards'); // Remove the class when hiding the cards
+    }
+}
