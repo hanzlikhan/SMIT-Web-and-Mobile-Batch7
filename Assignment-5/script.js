c10913da497e403012c34c2650fe99ed50ff6af4
hanzlikhan <khangormani79@gmail.com>
2024-02-29 23:56:23 +0500

    initialize

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -28,3 +28,40 @@ function toggleCards() {
         // hiddenCards.classList.remove('visible-cards'); // Remove the class when hiding the cards
     }
 }
+// Get a reference to the image element
+var image = document.getElementById("myImage");
+
+// Add event listeners for mouse enter and mouse leave
+image.addEventListener("mouseenter", function() {
+    // Change the image source on mouse enter
+    this.src = "./Assets/village.jpg"; // Replace 'hover-image.jpg' with the path to your hover image
+});
+
+image.addEventListener("mouseleave", function() {
+    // Change the image source back to original on mouse leave
+    this.src = "./Assets/AIPodcasteGenerator.cf943fff109f713cc4d2.png"; // Replace 'original-image.jpg' with the path to your original image
+});
+
+function showContent() {
+    document.getElementById("full_page").style.display = "block";
+    document.querySelector('.preloader').style.display = "none";
+  }
+  
+  // Event listener to call showContent function after 5 seconds
+  setTimeout(showContent, 2000);
+
+  var lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scroll down
+    document.getElementById("main-header").classList.add("hidden");
+  } else {
+    // Scroll up
+    document.getElementById("main-header").classList.remove("hidden");
+  }
+
+  lastScrollTop = currentScroll;
+});
