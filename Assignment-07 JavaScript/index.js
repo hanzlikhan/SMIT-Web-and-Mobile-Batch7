267593dbba55e0dac1cb6e04fef48c811a6efd01
hanzlikhan <khangormani79@gmail.com>
2024-04-04 08:49:14 +0500

    init

---
+++ Assignment-07 JavaScript/index.js
@@ -0,0 +1,36 @@
+// write a js program to find the maximum number b/w two numbers
+// var a = 5;
+// var b = 9;
+// if(a>b){
+//     console.log(`${a} is maximum`);
+// }else{
+//     console.log(`${b} is maximum`);
+// }
+
+// write a js program to find the maximum number b/w three numbers
+
+// var x = 4;
+// var y = 8;
+// var z = 12;
+// if(x > y && y > z){
+//     console.log(`${x} is maximum.`);
+// }
+// else if(y>x && y>z){
+//     console.log(`${y} is maximum.`);
+// }
+// else{
+//     console.log(`${z} is maximum.`);
+// }
+
+// write a js program to check wheather a number is negative,positive or zero.
+
+var n = 7;
+if(n>0){
+    console.log(`${n} is positive.`);
+}
+else if(n==0){
+    console.log(`${n} is positive.`);
+}
+else{
+    console.log(`${n} is negative.`);
+}
