9347fac9c76da6862291fb38e2d0e194d3360b1a
hanzlikhan <khangormani79@gmail.com>
2024-04-04 09:45:48 +0500

    init

--- Assignment-07 JavaScript/index.js
+++ Assignment-07 JavaScript/index.js
@@ -101,6 +101,92 @@ const readlineSync = require('readline-sync');
 
 // Write a js program to input the month number and print the number of
 // days in that month.
-var months = [31, 28, 31, 30,  31, 30, 31, 31, 30, 31, 30, 31];
-var mon = parseInt(readlineSync.question("Enter Month Number : "));
-console.log("Number of Days in this Month : ", months[mon - 1])
+// var months = [31, 28, 31, 30,  31, 30, 31, 31, 30, 31, 30, 31];
+// var mon = parseInt(readlineSync.question("Enter Month Number : "));
+// console.log("Number of Days in this Month : ", months[mon - 1])
+
+// Write a js program to count a minimum number of notes in a given
+// amount
+//  var amt = parseFloat(readlineSync.question("Enter Amount : "))  ;
+// var Notes = [50, 100, 500, 1000,5000];
+// var cnt = 0                    ;
+// for (let i = 0; i < Notes.length; i++) {            
+//     while (amt >= Notes[i]) {                          
+//         cnt++;       
+//         amt -= Notes[i];                 
+//     }                        
+// }                          
+// console.log("Minimum Number of Notes Required : ", cnt);
+
+// Write a js program to input marks of five subjects Physics, Chemistry,
+// Biology, Mathematics, and Computer. Calculate percentage and grade
+// according to the following:
+// Percentage >= 90% : Grade A
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+
+// var phy = parseFloat(readlineSync.question('Physics Marks : '));
+// var chem = parseFloat(readlineSync.question('Chemistry Marks : '));
+// var bio = parseFloat(readlineSync       .question('Biology Marks : '));
+// var math = parseFloat(readlineSync.question('Mathematics Marks : '));
+// var com = parseFloat(readlineSync.question('Computer Marks : '));
+// var total = phy + chem + bio + math + com;
+// var perc = (total / 5) * 100/100;
+// if(perc >= 90){
+//     console.log("Grade A");
+// }else if(perc>=80){
+//     console.log( "Grade B" );
+// }else if(perc>=70){
+//     console.log("Grade C");
+// }else if(perc>=60){
+//     console.log("Grade D");
+// }else if(perc>=40){
+//     console.log("Grade  E");
+// }else{
+//     console.log("Grade F");
+// }
+
+// Write a js program to input the basic salary of an employee and calculate
+// its Gross salary according to the following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+// Then calculate Gross Salary as follows:
+
+// Function to calculate gross salary
+// function calculateGrossSalary(basicSalary) {
+//     let hra, da;
+
+//     if (basicSalary <= 10000) {
+//         hra = 0.2 * basicSalary;
+//         da = 0.8 * basicSalary;
+//     } else if (basicSalary <= 20000) {
+//         hra = 0.25 * basicSalary;
+//         da = 0.9 * basicSalary;
+//     } else {
+//         hra = 0.3 * basicSalary;
+//         da = 0.95 * basicSalary;
+//     }
+
+//     const grossSalary = basicSalary + hra + da;
+//     return grossSalary;
+// }
+
+// // Get basic salary from the user
+
+// const basicSalary = parseFloat(readlineSync.question('Enter the basic salary: '));
+
+// // Calculate and display gross salary
+// const grossSalary = calculateGrossSalary(basicSalary);
+// console.log('Gross Salary:', grossSalary);
+
+// Write a js program to input electricity unit charges and calculate total
+// electricity bill according to the given condition:
+// For the first 50 units Rs. 0.50/unit
+// For the next 100 units Rs. 0.75/unit
+// For the next 100 units Rs. 1.20/unit
+// For units above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill
