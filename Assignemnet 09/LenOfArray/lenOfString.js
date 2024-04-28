26a5eaceb3482c1e0514e05bbc0c25f775927ef5
hanzlikhan <khangormani79@gmail.com>
2024-04-28 06:16:20 +0500

    Assignment -09

---
+++ Assignemnet 09/LenOfArray/lenOfString.js
@@ -0,0 +1,7 @@
+// a) Return the length of a given string.
+
+var str = "Muhammad Hanzla"
+function length(str) {
+    return str.length;
+}
+console.log(length(str));
---
+++ Assignemnet 09/concatString/concat.js
@@ -0,0 +1,9 @@
+// b) Concatenate two strings together.
+
+
+function concat(st1,str2) {
+    return st1 + str2
+}
+var str1 = "hanzla"
+var str2 = "gormani"
+console.log(concat(str1,str2));
