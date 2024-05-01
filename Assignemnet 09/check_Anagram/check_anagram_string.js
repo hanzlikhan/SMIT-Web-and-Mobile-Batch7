19e18bcd1d1432582984e454042e89c42e47f9e8
hanzlikhan <khangormani79@gmail.com>
2024-05-01 22:32:53 +0500

    init

--- Assignemnet 09/check_Anagram/check_anagram_string.js
+++ Assignemnet 09/check_Anagram/check_anagram_string.js
@@ -1,11 +1,17 @@
 // r) Check if a string is an anagram of another string.
 
-function isAnagram(str1,str2) {
-    str1 = str1.replace(' ','').toLowerCase().split('').sort().join('');
-    str2 = str2.replace(' ','').toLowerCase().split('').sort().join('');
+// Anagram :  ==>   anagram is a rearranged word or phrase that uses the same letters as the original word or phrase.
+//                    For example, "cinema" is an anagram of "iceman".
 
+//                    "Listen" is an anagram of "Silent".
+
+function isAnagram(str1,str2) {
+
+    str1 = str1.replace(" ","").toLowerCase().split('').sort().join('');
+    str2 = str2.replace(" ","").toLowerCase().split('').sort().join('');
+    
     return str1 === str2;
 }
-let str1 = "act"
+let str1 = "rc t"
 let str2 =  "cat"
-console.log((str1,str2));
+console.log(isAnagram(str1,str2));
