16f6b905a2805e2631f59eb728dfc3fcaaeb3533
hanzlikhan <khangormani79@gmail.com>
2024-05-16 02:40:09 +0500

    init

---
+++ Assgnment 10- Object-JavaScript/Task1-Object-Creation/ObjectCreation.js
@@ -0,0 +1,14 @@
+// Task 1: Object Creation
+
+// Create an empty object and assign it to a variable.
+// Create an object with properties for name, age, and profession.
+
+student ={
+    name: "Hanzla",
+    age: 21,
+    profession: "Student"
+}
+console.log(student);
+console.log("keys in the object :",Object.keys(student));
+console.log("Values in the object :",Object.values(student));
+console.log(student.name);
