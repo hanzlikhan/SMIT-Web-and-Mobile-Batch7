26d1a59792748df5321afaeca6bb8e8677202c38
hanzlikhan <khangormani79@gmail.com>
2024-05-16 02:47:29 +0500

    init

---
+++ Assgnment 10- Object-JavaScript/Task2-Object-Properties/Object-Properties.js
@@ -0,0 +1,13 @@
+// Task2-Object Propertie
+// Access and print the values of properties from an object.
+// Modify the value of a property in an object.
+// Add a new property to an existing object.
+student = {
+    name: "Hanzla",
+    Rollno: 221727,
+    profession: "Student"
+}
+student.Rollno = 221723
+student.cgpa = 3.7
+console.log(student)
+console.log("values of object :",Object.values(student))
