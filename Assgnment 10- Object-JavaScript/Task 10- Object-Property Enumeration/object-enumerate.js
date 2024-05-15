ce928be693938f674c1f705220505f1551946018
hanzlikhan <khangormani79@gmail.com>
2024-05-16 04:16:14 +0500

    init

---
+++ Assgnment 10- Object-JavaScript/Task 10- Object-Property Enumeration/object-enumerate.js
@@ -0,0 +1,14 @@
+// Task 10: Object Property Enumeration
+
+// Use Object.keys() to get an array of all the property names of an object.
+
+const student = {
+    name: 'hanzla',
+    age: 30,
+    city: 'DGkhan'
+  };
+  
+  const propertyNames = Object.keys(student);
+  
+  console.log(propertyNames); 
+  
