91f6a58418e922eb87a277f16384c0a882de82a6
hanzlikhan <khangormani79@gmail.com>
2024-05-17 01:18:45 +0500

    init

---
+++ Assignment 10 methods JavaScript/Math methods/mathfn.js
@@ -0,0 +1,50 @@
+        //  math methods in the javaScript
+
+
+    // 1- get the pi value 
+
+    var pi = Math.PI
+    console.log(pi) // 3.141592653589793
+    // 2- get the square root of a number
+    var squareRoot = Math.sqrt(9)
+    console.log(squareRoot) // 3
+
+    // 3- round off the value 
+    var roundOff = Math.round(2.5)
+    console.log(roundOff) // 3
+    
+    // 4- get the absolute value of a number
+    var absoluteValue = Math.abs(-2.5)
+    console.log(absoluteValue) // 2.5
+
+    // 5- get the maximum value of two numbers
+    var max = Math.max(2, 5)
+    console.log(max) // 5
+
+    // 6- get the minimum value of two numbers
+    var min = Math.min(2, 5)
+    console.log(min) // 2
+
+    // 7- get the random number between 0 and 1
+    var random = Math.random()
+    console.log(random) // 0.123456789
+
+    // 8- get the floor number
+    var floor = Math.floor(Math.random()*5)+1
+    console.log(floor) // 1, 2, 3, 4, 5
+
+    // 9- get the ceil number
+    var ceil = Math.ceil(Math.random()*5)+1
+    console.log(ceil) // 2, 3, 4, 5, 6
+
+    // 10- get the round number
+    var round = Math.round(Math.random()*5)+1
+    console.log(round) // 1, 2, 3, 4, 5
+
+    // 11- get the sin
+    var sin = Math.sin(2)
+    console.log(sin) // 0.9092974268256817
+
+    // 12- get the cos
+    var cos = Math.cos(2)
+    console.log(cos) // -0.4161468365471424
