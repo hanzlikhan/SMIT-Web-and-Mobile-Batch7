d484d14ebbbf7613ec0673ccc47fa78d78d1b136
hanzlikhan <khangormani79@gmail.com>
2024-06-22 12:39:45 +0500

    init

---
+++ Assignment 12/Scientific Calculator/src/util.js
@@ -0,0 +1,7 @@
+// UPDATE with other 2 parameter functions...
+
+export function notAMainFunction(operation) {
+    return operation != '+' && operation != '-'
+        && operation != '×' && operation != '÷'
+        && operation != 'xy' && operation != 'y√x';
+}
